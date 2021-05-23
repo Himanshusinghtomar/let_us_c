05840e611af467f963e89345efea911873186fdf
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2021-05-23 16:47:02 +0530

    Update Q02.c

--- Q02.c
+++ Q02.c
@@ -1,3 +1,5 @@
+/*The distance between two cities (in km.) is input through the keyboard. 
+Write a program to convert and print this distance in meters, feet, inches and centimeters.*/
 #include<stdio.h>
 
 int main()
